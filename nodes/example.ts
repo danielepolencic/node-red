d895efcdf8f9f0d1cea2ec5064b5e004253326cf
Daniele Polencic <daniele.polencic@gmail.com>
2020-07-06 21:42:30 +0800

    added typescript

--- nodes/example.ts
+++ nodes/example.ts
@@ -1,5 +1,7 @@
-module.exports = function (RED) {
-  function ExampleNode(config) {
+import { Red, Node, NodeProperties } from 'node-red'
+
+module.exports = function (RED: Red) {
+  function ExampleNode(this: Node, config: NodeProperties) {
     RED.nodes.createNode(this, config)
     const name = config.name
     const node = this
